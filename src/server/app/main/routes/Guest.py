f2532cea17792a45745508a73df6e8706918decc
Charul090 <baricharul9@hotmail.com>
2020-07-22 14:21:14 +0530

    Add guest register route

--- src/server/app/main/routes/Guest.py
+++ src/server/app/main/routes/Guest.py
@@ -1,5 +1,15 @@
 from . import guest
+from flask import request
+from ..services.guest import guest_register
+
 
 @guest.route("/login")
 def Login():
     return "Guest Login"
+
+
+@guest.route("/register", methods=["POST"])
+def Register():
+    response = guest_register(request.json)
+
+    return response
