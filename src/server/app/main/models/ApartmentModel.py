5e1ed1b80af1c7462702cfd12b64af7f8c3aa888
Charul090 <baricharul9@hotmail.com>
2020-07-23 22:36:34 +0530

    fix:Fixed Apartment Model

--- src/server/app/main/models/ApartmentModel.py
+++ src/server/app/main/models/ApartmentModel.py
@@ -3,12 +3,13 @@ from . import db
 
 class ApartmentModel(db.Model):
     __tablename__ = "apartment"
-    id = db.Column(db.Integer,primary_key=True)
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     type = db.Column(db.String(100))
     name = db.Column(db.String(100))
     country = db.Column(db.String(100))
     city = db.Column(db.String(100))
+    neighbourhood_area = db.Column(db.String(100))
     address1 = db.Column(db.String(1000))
     address2 = db.Column(db.String(1000))
     zipcode = db.Column(db.String(100))
@@ -18,4 +19,7 @@ class ApartmentModel(db.Model):
     local_facilities = db.Column(db.JSON)
     meals = db.Column(db.JSON)
     rules = db.Column(db.String(1000))
-    image = db.String(2000)
+    image = db.Column(db.String(2000))
+    deleted = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
