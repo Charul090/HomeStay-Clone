2d91443d8f59336f2e091890b0281a6cdd7835af
Lalitha Kunchala <lalithakunchala@gmail.com>
2020-07-24 00:26:47 +0530

    Navbar changes in home page

--- src/client/src/components/Carosel/Carosel.jsx
+++ src/client/src/components/Carosel/Carosel.jsx
@@ -7,51 +7,52 @@ export class Carosel extends Component {
     render() {
         return (
             <React.Fragment>
+                <NavBarHome />
                 <Carousel >
                     <Carousel.Item>
-                        <div style={{height:"500px"}}>
-                        <img
-                        className="d-block w-100  img-fluid"
-                        src="https://www.homestay.com/assets/homepage/hero/double-bed-next-to-bright-window-7f379bf25b6b842eccfcc25921e9f1c98fa5727582669ddd9d480aa1c49d48e5.jpg"
-                        alt="First slide"
-                        />
-                        </div>
-                        <Carousel.Caption style={{bottom:"300px"}}>
-                        <NavBarHome />
-                        <h2>Quality Rooms at
-                            Wallet-Friendly Prices</h2>
-                        <p>33,000 homestays in over country.</p>
+                            <div style={{height:"500px"}}>
+                                <img
+                                    className="d-block w-100  img-fluid"
+                                    src="https://www.homestay.com/assets/homepage/hero/double-bed-next-to-bright-window-7f379bf25b6b842eccfcc25921e9f1c98fa5727582669ddd9d480aa1c49d48e5.jpg"
+                                    alt="First slide"/>
+                            </div>
+                        <Carousel.Caption style={{bottom:"300px"}}> 
+                                <h2>Quality Rooms at
+                                    Wallet-Friendly Prices</h2>
+                                <p>33,000 homestays in over country.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
+
+                    <Carousel.Item>
+                            <div style={{height:"500px"}}>
+                                <img
+                                className="d-block w-100"
+                                src="https://www.homestay.com/assets/homepage/hero/double-bed-next-to-bright-window-7f379bf25b6b842eccfcc25921e9f1c98fa5727582669ddd9d480aa1c49d48e5.jpg"
+                                alt="Third slide"
+                                />
+                            </div>
+                        <Carousel.Caption style={{bottom:"300px"}}>
+                                <h2>Quality Rooms at
+                                    Wallet-Friendly Prices</h2>
+                                <p>33,000 homestays in over country.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+
                     <Carousel.Item>
                         <div style={{height:"500px"}}>
-                        <img
-                        className="d-block w-100"
-                        src="https://www.homestay.com/assets/homepage/hero/double-bed-next-to-bright-window-7f379bf25b6b842eccfcc25921e9f1c98fa5727582669ddd9d480aa1c49d48e5.jpg"
-                        alt="Third slide"
-                        />
+                            <img
+                            className="d-block w-100"
+                            src="https://www.homestay.com/assets/homepage/hero/double-bed-next-to-bright-window-7f379bf25b6b842eccfcc25921e9f1c98fa5727582669ddd9d480aa1c49d48e5.jpg"
+                            alt="Third slide"
+                            />
                         </div>
                         <Carousel.Caption style={{bottom:"300px"}}>
-                        <NavBarHome />
-                        <h3 className="mt-5">Second slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                <h2>Quality Rooms at
+                                    Wallet-Friendly Prices</h2>
+                                <p>33,000 homestays in over country.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
-                    <Carousel.Item>
-                        <div style={{height:"500px"}}>
-                        <img
-                        className="d-block w-100"
-                        src="https://www.homestay.com/assets/homepage/hero/double-bed-next-to-bright-window-7f379bf25b6b842eccfcc25921e9f1c98fa5727582669ddd9d480aa1c49d48e5.jpg"
-                        alt="Third slide"
-                        />
-                        </div>
-                        <Carousel.Caption style={{bottom:"300px"}}>
-                        <NavBarHome />
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-            </Carousel>
+                </Carousel>
 </React.Fragment>
 
         )
