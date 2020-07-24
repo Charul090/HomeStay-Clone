a767a99b36ab058d1b5c8a3bf162ff39c9499d04
Charul090 <baricharul9@hotmail.com>
2020-07-24 14:20:24 +0530

    Integrated google oauth

--- src/client/src/redux/AuthRedux/action.js
+++ src/client/src/redux/AuthRedux/action.js
@@ -46,4 +46,49 @@ const Start_Login_Query = (data)=>{
     }
 }
 
-export {Start_Login_Query}
+const GOOGLE_QUERY = ()=>{
+    return {
+        type:SEND_GOOGLE_QUERY
+    }
+}
+
+const GOOGLE_SUCCESS = (payload) => {
+    return {
+        type: LOGIN_GOOGLE_SUCCESSFULL,
+        payload
+    }
+}
+
+const GOOGLE_FAILURE = (payload) => {
+    return {
+        type: LOGIN_GOOGLE_FAIL,
+        payload
+    }
+}
+
+const Google_Login_Query=(data)=>{
+    return dispatch=>{
+        dispatch(GOOGLE_QUERY())
+        return axios({
+            method: "post",
+            baseURL:"http://0f3cf9485c74.ngrok.io",
+            url: "/oauth/google",
+            data: data
+        })
+        .then((payload)=>payload.data)
+        .then((data)=>{
+            if(data["error"]){
+                dispatch(GOOGLE_FAILURE(data))
+            }
+            else{
+                dispatch(GOOGLE_SUCCESS(data))
+            }
+
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+}
+
+export {Start_Login_Query,Google_Login_Query}
