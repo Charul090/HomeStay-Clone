921be4fb1d77b56353d3a3b40697695c1c5ed6f7
Charul090 <baricharul9@hotmail.com>
2020-07-25 18:20:18 +0530

    Added Area & Reviews Info Route

--- src/server/app/main/routes/Apartment.py
+++ src/server/app/main/routes/Apartment.py
@@ -1,9 +1,15 @@
 from . import apartment
 from flask import request
-from ..services.apartment import basic_info
+from ..services.apartment import basic_info, area_info
 
 
 @apartment.route("/info/<id>")
 def basic(id):
     response = basic_info(id)
+    return response
+
+
+@apartment.route("/area-info/<id>")
+def area(id):
+    response = area_info(id)
     return response
