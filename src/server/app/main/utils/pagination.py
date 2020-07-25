f4acaa3b55f1b85a04fcd5034ae906fa57dc08c9
Charul090 <baricharul9@hotmail.com>
2020-07-25 18:45:02 +0530

    Fixed pagination logic

--- src/server/app/main/utils/pagination.py
+++ src/server/app/main/utils/pagination.py
@@ -4,6 +4,10 @@ from math import ceil
 def pagination(length, page):
     per_page = 10
     total_pages = ceil(length/per_page)
+
+    if page > total_pages:
+        return False,"",""
+
     start = (page - 1)*per_page
     end = page * per_page
 
