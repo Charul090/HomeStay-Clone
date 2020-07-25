aa27c7ff743dba6bb8231ad2aea01b9aa910d919
Charul090 <baricharul9@hotmail.com>
2020-07-25 10:47:54 +0530

    Added HomePromoBox component

--- src/client/src/components/Home/Home.jsx
+++ src/client/src/components/Home/Home.jsx
@@ -4,15 +4,20 @@ import RegisterModal from "../RegisterModal/RegisterModal"
 import LoginModal from '../LoginModal/LoginModal';
 import TransparentNavBar from "../TransparentNavBar/TransparentNavBar.jsx"
 import SecondaryNavBar from '../SecondaryNavBar/SecondaryNavBar';
+import HomeReviewPanel from '../HomeReviewPanel/HomeReviewPanel';
+import styles from "./Home.module.css"
+import HomePromoBox from '../HomePromoBox/HomePromoBox';
 
 export default function Home() {
     return (
-        <>
+        <div className={styles.body}>
             <TransparentNavBar />
             <LoginModal />
             <RegisterModal />
             <HomeHero />
             <SecondaryNavBar />
-        </>
+            <HomeReviewPanel />
+            <HomePromoBox />
+        </div>
     )
 }
