6ee42c4f35b1c38433f076b484bcea3467cc01cc
Charul090 <baricharul9@hotmail.com>
2020-07-26 11:56:08 +0530

    Added Apartment registration route

--- src/server/app/main/routes/Host.py
+++ src/server/app/main/routes/Host.py
@@ -1,11 +1,15 @@
 from . import host
 from flask import request
 from ..services.host import host_login
+from ..services.listing import add_apartment,add_bedroom,add_host_profile,add_location,add_reviews
 
 
 @host.route("/login", methods=["POST"])
 def Login():
     response = host_login(request.json)
-
     return response
 
+@host.route("/register")
+def Register():
+    add_reviews()
+    return "Success"
