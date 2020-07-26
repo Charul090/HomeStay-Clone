a844a46018b98ce4dd0f76667f5ae9a02b7ad955
Charul090 <baricharul9@hotmail.com>
2020-07-26 12:33:56 +0530

    Added route for booking info api

--- src/server/app/main/routes/Apartment.py
+++ src/server/app/main/routes/Apartment.py
@@ -1,6 +1,6 @@
 from . import apartment
 from flask import request
-from ..services.apartment import basic_info, area_info
+from ..services.apartment import basic_info, area_info,booking_info
 
 
 @apartment.route("/info/<id>")
@@ -12,4 +12,10 @@ def basic(id):
 @apartment.route("/area-info/<id>")
 def area(id):
     response = area_info(id)
+    return response
+
+
+@apartment.route("/booking-info/<id>")
+def booking(id):
+    response = booking_info(id)
     return response
