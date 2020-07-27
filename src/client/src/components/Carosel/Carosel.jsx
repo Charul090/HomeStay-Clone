fd65178d8d799bbf060f35a9e826f3314de8c20a
Charul090 <baricharul9@hotmail.com>
2020-07-27 10:38:05 +0530

    Added Hero Component

--- src/client/src/components/Carosel/Carosel.jsx
+++ src/client/src/components/Carosel/Carosel.jsx
@@ -1,17 +1,19 @@
-import NavBarHome from "../NavBarHome/NavBarHome.jsx";
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { Carousel } from 'react-bootstrap';
+import PrevArrow from "../PrevArrow/PrevArrow.jsx"
+import NextArrow from "../NextArrow/NextArrow.jsx"
+import styles from "./Carosel.module.css"
 
 export class Carosel extends Component {
     render() {
         return (
             <React.Fragment>
-                <Carousel indicators={false} slide={false} fade={true}>
+                <Carousel className={styles.main} indicators={false} prevIcon={<PrevArrow />} nextIcon={<NextArrow />}>
                     <Carousel.Item>
                         <div>
                             <img
-                                className="d-block w-100  img-fluid"
+                                className="d-block w-100"
                                 src="https://www.homestay.com/assets/homepage/hero/double-bed-next-to-bright-window-7f379bf25b6b842eccfcc25921e9f1c98fa5727582669ddd9d480aa1c49d48e5.jpg"
                                 alt="First slide" />
                         </div>
@@ -19,7 +21,7 @@ export class Carosel extends Component {
                     <Carousel.Item>
                         <div>
                             <img
-                                className="d-block w-100"
+                                className="d-block w-100" 
                                 src="https://www.homestay.com/assets/homepage/hero/double-bed-with-bed-locker-and-wardrobes-ed8c9bd5df88db08f90424467ef902df4f301b492dee25b3fdbf88e1b88aebed.jpg"
                                 alt="Second slide"
                             />
---
+++ src/client/src/components/PrevArrow/PrevArrow.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./PrevArrow.module.css"
+
+export default function PrevArrow() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.prev}>
+                
+            </div>
+        </div>
+    )
+}
