466d398851968ec79cdaaed8b1257d0cd29d47bd
Charul090 <baricharul9@hotmail.com>
2020-07-28 14:49:14 +0530

    Added area info redux flow

--- src/client/src/redux/EntityRedux/action.js
+++ src/client/src/redux/EntityRedux/action.js
@@ -1,4 +1,4 @@
-import {BASIC_QUERY,BASIC_QUERY_SUCCESS,BASIC_QUERY_FAIL,BOOKING_QUERY,BOOKING_QUERY_SUCCESS,BOOKING_QUERY_FAIL} from "./actiontypes.js"
+import {BASIC_QUERY,BASIC_QUERY_SUCCESS,BASIC_QUERY_FAIL,BOOKING_QUERY,BOOKING_QUERY_SUCCESS,BOOKING_QUERY_FAIL,AREA_QUERY,AREA_QUERY_SUCCESS,AREA_QUERY_FAIL} from "./actiontypes.js"
 import axios from "axios"
 
 const SEND_BASIC_QUERY = ()=>{
@@ -37,6 +37,26 @@ const BOOKING_API_SUCCESS=(payload)=>{
 const BOOKING_API_FAILURE = (payload)=>{
     return {
         type:BOOKING_QUERY_FAIL,
+        payload
+    }
+}
+
+const SEND_AREA_QUERY = ()=>{
+    return {
+        type:AREA_QUERY
+    }
+}
+
+const AREA_API_SUCCESS=(payload)=>{
+    return{
+        type:AREA_QUERY_SUCCESS,
+        payload
+    }
+}
+
+const AREA_API_FAILURE = (payload)=>{
+    return {
+        type:AREA_QUERY_FAIL,
         payload
     }
 }
@@ -80,6 +100,30 @@ const Start_Booking_Query = (id)=>{
             }
             else{
                 dispatch(BOOKING_API_SUCCESS(data))
+                dispatch(Start_Area_Query(id))
+            }
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+}
+
+const Start_Area_Query = (id)=>{
+    return dispatch=>{
+        dispatch(SEND_AREA_QUERY())
+        return axios({
+            method:"get",
+            baseURL:"http://0a4399bd8526.ngrok.io",
+            url:`/apartment/area-info/${id}`
+        })
+        .then((res)=>res.data)
+        .then((data)=>{
+            if(data["error"]){
+                dispatch(AREA_API_FAILURE(data))
+            }
+            else{
+                dispatch(AREA_API_SUCCESS(data))
             }
         })
         .catch((err)=>{
