18a2a4f274ec7979f8c095d30a8dcdb756358b31
Charul090 <baricharul9@hotmail.com>
2020-07-28 13:29:47 +0530

    Added BedroomInfo Component

--- src/client/src/components/Entity/Entity.jsx
+++ src/client/src/components/Entity/Entity.jsx
@@ -3,11 +3,13 @@ import ApartmentInfo from '../ApartmentInfo/ApartmentInfo.jsx'
 import styles from "./Entity.module.css"
 import NavBarHome from "../NavBarHome/NavBarHome.jsx"
 import Footer from "../Footer/Footer.jsx"
+import BedroomInfo from '../BedroomInfo/BedroomInfo.jsx'
 
 export default function Entity() {
     return (
         <div className={styles.main}>
             <ApartmentInfo />
+            <BedroomInfo />
             <Footer />
         </div>
     )
