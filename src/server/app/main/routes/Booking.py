ed6f2c584e530d93435be0eb4adbc5bba61c5e5f
Charul090 <baricharul9@hotmail.com>
2020-07-29 16:27:45 +0530

    Added check booking route

--- src/server/app/main/routes/Booking.py
+++ src/server/app/main/routes/Booking.py
@@ -1,9 +1,13 @@
 from . import booking
 from flask import request
-from ..services.booking import set_booking
+from ..services.booking import set_booking,check_booking
 
 @booking.route("/add",methods=["POST"])
-def booking():
+def book():
     response = set_booking(request.json)
     return response
 
+@booking.route("/check/<id>")
+def status(id):
+    response = check_booking(id,request.args)
+    return response
