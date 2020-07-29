71a69d650efd696932ec1fdb9b8aa3729d70bdee
Charul090 <baricharul9@hotmail.com>
2020-07-29 15:58:39 +0530

    Edited font awesom link

---
+++ src/client/src/redux/BookingRedux/action.js
@@ -0,0 +1,55 @@
+import { CHECK_DATES_QUERY, CHECK_DATES_SUCCESS, CHECK_DATES_FAIL, SET_QUERY_FALSE } from "./actiontypes.js"
+import axios from "axios"
+
+const DATES_QUERY=()=>{
+    return {
+        type:CHECK_DATES_QUERY
+    }
+}
+
+const DATES_SUCCESS=(payload)=>{
+    return {
+        type:CHECK_DATES_SUCCESS,
+        payload
+    }
+}
+
+const DATES_FAIL=(payload)=>{
+    return {
+        type:CHECK_DATES_FAIL,
+        payload
+    }
+}
+
+const QUERY_COMPLETE=()=>{
+    return {
+        type:SET_QUERY_FALSE
+    }
+}
+
+const SEND_CHECK_QUERY=(obj)=>{
+    let {id,start,end} = obj
+    return dispatch=>{
+        dispatch(DATES_QUERY())
+        axios({
+            method:"get",
+            baseURL:"http://7e9638a87721.ngrok.io",
+            url:`/booking/check/${id}`,
+            params:{
+                start,
+                end
+            }
+        })
+        .then((payload)=>payload.data)
+        .then((data)=>{
+            if(data["status"]){
+                dispatch(DATES_SUCCESS(data))
+            }
+            else{
+                dispatch(DATES_FAIL(data))
+            }
+        })
+    }
+}
+
+export {SEND_CHECK_QUERY,QUERY_COMPLETE}
--- src/client/src/redux/store.js
+++ src/client/src/redux/store.js
@@ -2,10 +2,11 @@ import AuthReducer from "./AuthRedux/reducer";
 import RegisterReducer from "./RegisterRedux/reducer"
 import reducer from "./ListRedux/reducer";
 import EntityReducer from "./EntityRedux/reducer"
+import BookingReducer from "./BookingRedux/reducer"
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
-const rootReducer = combineReducers({ auth:AuthReducer,regis:RegisterReducer,list:reducer,entity:EntityReducer});
+const rootReducer = combineReducers({ auth:AuthReducer,regis:RegisterReducer,list:reducer,entity:EntityReducer,booking:BookingReducer});
 
 
 let composeEnhancers = compose;
