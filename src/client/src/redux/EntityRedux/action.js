36d3babdc1bd2924a4e5cef8663c4c5abc47e34c
Charul090 <baricharul9@hotmail.com>
2020-07-29 16:26:59 +0530

    Added Redux for recommendation

--- src/client/src/redux/EntityRedux/action.js
+++ src/client/src/redux/EntityRedux/action.js
@@ -1,4 +1,4 @@
-import {BASIC_QUERY,BASIC_QUERY_SUCCESS,BASIC_QUERY_FAIL,BOOKING_QUERY,BOOKING_QUERY_SUCCESS,BOOKING_QUERY_FAIL,AREA_QUERY,AREA_QUERY_SUCCESS,AREA_QUERY_FAIL} from "./actiontypes.js"
+import {BASIC_QUERY,BASIC_QUERY_SUCCESS,BASIC_QUERY_FAIL,BOOKING_QUERY,BOOKING_QUERY_SUCCESS,BOOKING_QUERY_FAIL,AREA_QUERY,AREA_QUERY_SUCCESS,AREA_QUERY_FAIL,RECOMMEND_QUERY,RECOMMEND_QUERY_SUCCESS,RECOMMEND_QUERY_FAIL} from "./actiontypes.js"
 import axios from "axios"
 
 const SEND_BASIC_QUERY = ()=>{
@@ -57,6 +57,26 @@ const AREA_API_SUCCESS=(payload)=>{
 const AREA_API_FAILURE = (payload)=>{
     return {
         type:AREA_QUERY_FAIL,
+        payload
+    }
+}
+
+const SEND_RECOMMEND_QUERY = ()=>{
+    return {
+        type:RECOMMEND_QUERY
+    }
+}
+
+const RECOMMEND_API_SUCCESS=(payload)=>{
+    return{
+        type:RECOMMEND_QUERY_SUCCESS,
+        payload
+    }
+}
+
+const RECOMMEND_API_FAILURE = (payload)=>{
+    return {
+        type:RECOMMEND_QUERY_FAIL,
         payload
     }
 }
@@ -124,6 +144,30 @@ const Start_Area_Query = (id)=>{
             }
             else{
                 dispatch(AREA_API_SUCCESS(data))
+                dispatch(Start_Recommend_Query(id))
+            }
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+}
+
+const Start_Recommend_Query = (id)=>{
+    return dispatch=>{
+        dispatch(SEND_RECOMMEND_QUERY())
+        return axios({
+            method:"get",
+            baseURL:"https://7e9638a87721.ngrok.io",
+            url:`/apartment/recommend/${id}`
+        })
+        .then((res)=>res.data)
+        .then((data)=>{
+            if(data["error"]){
+                dispatch(RECOMMEND_API_FAILURE(data))
+            }
+            else{
+                dispatch(RECOMMEND_API_SUCCESS(data))
             }
         })
         .catch((err)=>{
