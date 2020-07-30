85864b6c27bece70edb774fc8a9af8b8786e0635
Charul090 <baricharul9@hotmail.com>
2020-07-30 17:35:13 +0530

    Updated booking service

--- src/server/app/main/services/booking.py
+++ src/server/app/main/services/booking.py
@@ -7,21 +7,28 @@ import random
 
 def set_booking(info):
     ## logic for converting start time and end time into datetime object pending
+    start = info["start"]
+    end = info["end"]
+
+    start = datetime.datetime.strptime(start,'%Y-%m-%dT%H:%M:%S.%fZ')
+    end = datetime.datetime.strptime(end,'%Y-%m-%dT%H:%M:%S.%fZ')
 
     apartment_check = ApartmentModel.query.filter(ApartmentModel.id == info["apartment_id"]).first()
 
     if apartment_check is None:
-        return json.dumps({"error":True,"message":"Apartment id Invalid"})
+        return json.dumps({"error":True,"message":"Apartment id Invalid","booking":False})
 
     date_check = BookingModel.query.filter(BookingModel.apartment_id == info["apartment_id"]).all()
 
     for x in date_check:
-        if info["start_time"] >= date_check.start_date and info["start_time"] <= date_check.end_date:
-            return json.dumps({"error":True,"message":"Date already booked"})
+        if start >= x.start_date and end <= x.end_date:
+            return json.dumps({"error":True,"message":"Date already booked","booking":False})
     
-    booking = BookingModel(apartment_id=info["apartment_id"],user_id=info["user_id"],start_date=info["start_date"],end_date=info["end_date"],total_cost=info["total_cost"])
+    booking = BookingModel(apartment_id=info["apartment_id"],start_date=start,end_date=end,total_cost=info["total_cost"])
+    db.session.add(booking)
+    db.session.commit()
 
-    return json.dumps({"error":False,"message":"Booking Successfull"})
+    return json.dumps({"error":False,"message":"Booking Successfull","booking":True})
 
 def check_booking(id,info):
 
