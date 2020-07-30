2df082c3ddfd0940cdb083e7e089d61fb2721ac0
Charul090 <baricharul9@hotmail.com>
2020-07-30 17:34:48 +0530

    Added payment route

--- src/server/app/main/routes/Payment.py
+++ src/server/app/main/routes/Payment.py
@@ -1,9 +1,22 @@
 from flask import request
 from . import payment
-from ..services.payment import process_payment
+from ..services.payment import process_payment, process_verify,payment_verify
 
 
-@payment.route("/order",methods=["POST"])
+@payment.route("/order", methods=["POST"])
 def start_order():
     response = process_payment(request.data)
+    return response
+
+
+@payment.route("/verify", methods=["POST"])
+def verify_order():
+    webhook_signature = request.headers.get("x-razorpay-signature")
+    response = process_verify(request.data,webhook_signature)
+    return response
+
+
+@payment.route("/authenticate",methods=["POST"])
+def authenticate_order():
+    response = payment_verify(request.json)
     return response
