dbd3575ebb04fe6ec310084d8d17c66ccb5020d5
Charul090 <baricharul9@hotmail.com>
2020-07-31 03:18:40 +0530

    Added google autosearch functionality

--- src/client/src/components/HomeHero/HomeHero.jsx
+++ src/client/src/components/HomeHero/HomeHero.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from "./HomeHero.module.css"
 import { Carosel } from '../Carosel/Carosel'
-import HeroBanner from '../HeroBanner/HeroBanner'
 
 export default function HomeHero() {
     return (
