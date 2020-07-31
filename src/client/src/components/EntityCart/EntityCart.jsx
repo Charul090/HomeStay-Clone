e0a25a8b78cf329bd0f67af3a9b71569f066f2da
Charul090 <baricharul9@hotmail.com>
2020-07-31 10:18:27 +0530

    Fixed payment bug

--- src/client/src/components/EntityCart/EntityCart.jsx
+++ src/client/src/components/EntityCart/EntityCart.jsx
@@ -58,7 +58,7 @@ export default function EntityCart(props) {
 
         }
 
-        const data = await fetch("https://8431a7cdc386.ngrok.io/payment/order", {
+        const data = await fetch("https://1f709f3ba43e.ngrok.io/payment/order", {
             method: "POST",
             body: JSON.stringify(order)
         }).then((data) => {
