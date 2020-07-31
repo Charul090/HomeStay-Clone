8105d6a1f4417af89a8897a730cc8fa5ded5a468
Lalitha Kunchala <lalithakunchala@gmail.com>
2020-07-31 18:30:20 +0530

    reviews completed

--- src/client/src/components/Reviews/Reviews.jsx
+++ src/client/src/components/Reviews/Reviews.jsx
@@ -1,30 +1,44 @@
 import React from 'react'
 import styles from './Reviews.module.css'
+import { useSelector } from "react-redux";
 
 export default function Reviews() {
+    let { reviews } = useSelector(state => state.entity);
+    console.log(reviews);
+    const data = []
+    for(var key in reviews){
+        data.push(reviews[key]);
+    }
     return (
-        <div>
-            <div className="container">
-                <div className="row">
+        <div className={styles.spacer1}>
+            {data&& data.map((item)=>(<div className="container ">
+                <h3 className="text-left">Reviews</h3>
+                <div className="row ">
                     <div className="col-8">
-                        <div className="row">
-                            <div className="col-3 p-3 ">
-                                <div>
-                                <img className = {styles.imgheight} src="https://www.homestay.com/assets/user/graphic-avatar-placeholder-0f9a1eaaaad293c274aadb54ef4776c60ecb1d178e712b69b2b81002862e81f6.svg" alt="image"/>
+                        <div className="row mt-2 p-2">
+                            <div className="col-2 ">
+                                <div className="pr-2">
+                                <img className = {styles.imgheight} src={item.profile_pic} alt="image"/>
                                 </div>
-                                <p class="muted">Italy</p>
-                                <p class="muted">Age : 45</p>
+                                <h5 className="text-primary">{item.name}</h5>
+                                <div class="text-muted ">Italy</div>
+                                <div class="text-muted " >Age: 45</div>
                             </div>
-                            <div className="col-9">
-                                <div class="row">
-                                <div className="text-left col-6">left</div>
-                                <div className="float-right col-6">right</div>
+                            <div className="col-10 pl-5" style={{background:"#F1EFEF",borderRadius: "15px"}}>
+                                <div className={styles.arrowBox}>
+                                <div class="row pt-3">
+                                <div className="text-left text-muted col-6">August 2020</div>
+                                <div className="float-right col-6"><img src="https://www.homestay.com/assets/small-review-score-img-c68dbe08abd3620cb7ae9b367952c1caac5322eb67c4d65ecfcafd83fba88dda.svg"/></div>
+                                <span className="pt-3">
+                                <div className={styles.para} >{item.review}</div>
+                                </span>
+                                </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>))}
         </div>
     )
 }
