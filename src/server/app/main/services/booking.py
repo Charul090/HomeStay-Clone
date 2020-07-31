d9229d0a2eb640029cc84cfedbdc928e196ff0bf
Charul090 <baricharul9@hotmail.com>
2020-07-31 20:00:34 +0530

    Added token authectication

--- src/server/app/main/services/booking.py
+++ src/server/app/main/services/booking.py
@@ -1,14 +1,27 @@
 import json
 import jwt
-from ..models import db,BookingModel,ApartmentModel
+from ..models import db,BookingModel,ApartmentModel,UsersModel
 import datetime
 import random
+from ..utils.token import decode_token
 
 
 def set_booking(info):
     ## logic for converting start time and end time into datetime object pending
     start = info["start"]
     end = info["end"]
+
+    email = decode_token(info["token"])
+
+    if email is False:
+        return json.dumps({"error":True,"message":"Login token expired","booking":False})
+    
+    user_query = UsersModel.query.filter(UsersModel.email == email).first()
+
+    if user_query is None:
+        return json.dumps({"error":True,"message":"Email is invalid","booking":False})
+    
+
 
     start = datetime.datetime.strptime(start,'%Y-%m-%dT%H:%M:%S.%fZ')
     end = datetime.datetime.strptime(end,'%Y-%m-%dT%H:%M:%S.%fZ')
@@ -24,7 +37,7 @@ def set_booking(info):
         if start >= x.start_date and end <= x.end_date:
             return json.dumps({"error":True,"message":"Date already booked","booking":False})
     
-    booking = BookingModel(apartment_id=info["apartment_id"],start_date=start,end_date=end,total_cost=info["total_cost"])
+    booking = BookingModel(apartment_id=info["apartment_id"],start_date=start,end_date=end,total_cost=info["total_cost"],user_id=user_query.id)
     db.session.add(booking)
     db.session.commit()
 
