99d5c6e93d695af0f7c57efdac2932c38b38c8a4
Charul090 <baricharul9@hotmail.com>
2020-07-31 19:39:56 +0530

    Added Login and resgister modal for entity page

--- src/client/src/components/NavBarHome/NavBarHome.jsx
+++ src/client/src/components/NavBarHome/NavBarHome.jsx
@@ -42,18 +42,12 @@ const NavBarHome = () => {
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                       </NavDropdown>
                       <NavDropdown title="sign up" className="sec" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        <NavDropdown.Item href="#registermodal" data-toggle="modal">Guest Signup</NavDropdown.Item>
+                        <NavDropdown.Item href="#">Host Signup</NavDropdown.Item>
                       </NavDropdown>
                       <NavDropdown title="log in" className="sec" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        <NavDropdown.Item href="#loginmodal" data-toggle="modal" data-target="#loginmodal">Guest Login</NavDropdown.Item>
+                        <NavDropdown.Item href="#loginmodal" data-toggle="modal" data-target="#loginmodal">Host Login</NavDropdown.Item>
                       </NavDropdown>
                       <NavDropdown title="help" className="sec" id="collasible-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
