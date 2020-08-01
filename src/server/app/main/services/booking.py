e00ce1916555df445c3b94001b9833acd037ca6a
Charul090 <baricharul9@hotmail.com>
2020-08-01 15:49:56 +0530

    Added email booking confirmation functionality to booking servcie

--- src/server/app/main/services/booking.py
+++ src/server/app/main/services/booking.py
@@ -4,6 +4,8 @@ from ..models import db,BookingModel,ApartmentModel,UsersModel
 import datetime
 import random
 from ..utils.token import decode_token
+import smtplib
+from instance.config import SMTP_USERNAME, SMTP_PASSWORD
 
 
 def set_booking(info):
@@ -21,6 +23,14 @@ def set_booking(info):
     if user_query is None:
         return json.dumps({"error":True,"message":"Email is invalid","booking":False})
     
+    
+    s = smtplib.SMTP(host="email-smtp.ap-south-1.amazonaws.com")
+
+    s.connect("email-smtp.ap-south-1.amazonaws.com", 587)
+
+    s.starttls()
+
+    s.login(SMTP_USERNAME, SMTP_PASSWORD)
 
 
     start = datetime.datetime.strptime(start,'%Y-%m-%dT%H:%M:%S.%fZ')
@@ -40,6 +50,12 @@ def set_booking(info):
     booking = BookingModel(apartment_id=info["apartment_id"],start_date=start,end_date=end,total_cost=info["total_cost"],user_id=user_query.id)
     db.session.add(booking)
     db.session.commit()
+    
+    booking_query = BookingModel.query.filter(BookingModel.apartment_id == info["apartment_id"],BookingModel.start_date == start).first()
+
+    msg = "From:noreply@charul.co\nTo:"+str(email)+"\nSubject:Booking Confirmation\n\nHello,\nFollowing are the booking details.Happy Homestaying!\nPayment id: "+str(info["response"]["razorpay_order_id"])+"\nBooking Id: "+str(booking_query.id)+"\nName: "+str(user_query.firstname)+"\nEmail: "+str(email)+"\nTotal Cost: Rs."+str(info["total_cost"])+"\nStart Date: "+str(start.strftime("%d/%m/%Y"))+"\nEnd Date: "+str(end.strftime("%d/%m/%Y"))
+    
+    s.sendmail("noreply@charul.co",email,msg)
 
     return json.dumps({"error":False,"message":"Booking Successfull","booking":True})
 
